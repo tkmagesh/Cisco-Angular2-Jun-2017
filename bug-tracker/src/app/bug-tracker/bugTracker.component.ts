8c1cc0aa00fbc00dded80a198d80a4e363c6c063
Magesh <tkmagesh77@gmail.com>
2017-06-28 11:46:43 +0530

    bug tracker - initial

---
+++ bug-tracker/src/app/bug-tracker/bugTracker.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IBug } from './models/IBug';
+
+@Component({
+	selector : 'bug-tracker',
+	templateUrl : 'bugTracker.component.html',
+	styleUrls : ['bugTracker.component.css']
+})
+export class BugTrackerComponent{
+
+	bugs : Array<IBug> = [];
+
+	onAddNewClick(bugName : string){
+		const newBug = {
+			name : bugName,
+			isClosed : false
+		};
+		this.bugs.push(newBug);
+	}
+
+	onBugClick(){
+		console.log('a bug is being clicked');
+	}
+}
+
